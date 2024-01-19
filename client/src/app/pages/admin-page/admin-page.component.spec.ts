f59610c1df79a009c42432dcea9de59dfeeb69fd
Youva <youva.boutora@polymtl.ca>
2024-01-19 11:26:38 -0500

    Ajout des pages pour la vue initiale.

---
+++ client/src/app/pages/admin-page/admin-page.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { PlayAreaComponent } from '@app/components/play-area/play-area.component';
+import { SidebarComponent } from '@app/components/sidebar/sidebar.component';
+import { AdminPageComponent } from './admin-page.component';
+
+describe('CreateGamePageComponent', () => {
+    let component: AdminPageComponent;
+    let fixture: ComponentFixture<AdminPageComponent>;
+
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [AdminPageComponent, SidebarComponent, PlayAreaComponent],
+        }).compileComponents();
+    });
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(AdminPageComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+});
---
+++ client/src/app/pages/create-game-page/create-game-page.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { PlayAreaComponent } from '@app/components/play-area/play-area.component';
+import { SidebarComponent } from '@app/components/sidebar/sidebar.component';
+import { CreateGamePageComponent } from './create-game-page.component';
+
+describe('CreateGamePageComponent', () => {
+    let component: CreateGamePageComponent;
+    let fixture: ComponentFixture<CreateGamePageComponent>;
+
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [CreateGamePageComponent, SidebarComponent, PlayAreaComponent],
+        }).compileComponents();
+    });
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(CreateGamePageComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+});
