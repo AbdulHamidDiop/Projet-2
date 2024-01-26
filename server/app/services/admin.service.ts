fb1ee075fda611d63f434c702cb2b02e97cd543f
aantem <alexandre.antem@polymtl.ca>
2024-01-26 10:07:04 -0500

    Bouger les services concernant jeux et questions dans adminService a questionsService

--- server/app/services/admin.service.ts
+++ server/app/services/admin.service.ts
@@ -1,10 +1,7 @@
-import { Game } from '@common/game';
 import { Message } from '@common/message';
-import * as fs from 'fs/promises';
 import { Service } from 'typedi';
 
 const PASSWORD = 'LOG2990-312';
-const QUIZ_PATH = './assets/quiz-example.json';
 
 @Service()
 export class AdminService {
@@ -13,38 +10,5 @@ export class AdminService {
             title: 'Correct Password',
             body: (password === PASSWORD).toString(),
         };
-    }
-
-    async addGame(game: Game): Promise<void> {
-        const games: Game[] = await this.getAllGames();
-        games.push(game);
-        await fs.writeFile(QUIZ_PATH, JSON.stringify(games, null, 2), 'utf8');
-    }
-
-    async getAllGames(): Promise<Game[]> {
-        const data: string = await fs.readFile(QUIZ_PATH, 'utf8');
-        const games: Game[] = JSON.parse(data);
-        return games;
-    }
-
-    async getGameByID(id: string): Promise<Game> {
-        const games: Game[] = await this.getAllGames();
-        return games.find((game) => game.id === id);
-    }
-
-    async toggleHidden(id: string): Promise<boolean> {
-        const games: Game[] = await this.getAllGames();
-        const updatedGames: Game[] = games.map((game) =>
-            game.id === id ? { ...game, lastModification: new Date(), isHidden: !game.isHidden } : game,
-        );
-        await fs.writeFile(QUIZ_PATH, JSON.stringify(updatedGames, null, 2), 'utf8');
-        return true;
-    }
-
-    async deleteGameByID(id: string): Promise<boolean> {
-        const games: Game[] = await this.getAllGames();
-        const updatedGames: Game[] = games.filter((game) => game.id !== id);
-        await fs.writeFile(QUIZ_PATH, JSON.stringify(updatedGames, null, 2), 'utf8');
-        return true;
     }
 }
--- server/app/services/questions.service.ts
+++ server/app/services/questions.service.ts
@@ -1,40 +1,54 @@
-import { Question } from '@common/game';
+import { Game, Question } from '@common/game';
 import * as fs from 'fs/promises';
 import { Service } from 'typedi';
 
-const QUESTIONS_PATH = "./assets/questions-database.json"
+const QUESTIONS_PATH = './assets/questions-database.json';
+const QUIZ_PATH = './assets/quiz-example.json';
 
 @Service()
 export class QuestionsService {
     async getAllQuestions(): Promise<Question[]> {
-        try {
-            const data: string = await fs.readFile(QUESTIONS_PATH, 'utf8');
-            const questions: Question[] = JSON.parse(data);
-            return questions;
-        } catch (error) {
-            throw error;
-        }
+        const data: string = await fs.readFile(QUESTIONS_PATH, 'utf8');
+        const questions: Question[] = JSON.parse(data);
+        return questions;
     }
 
     async sortAllQuestions(): Promise<Question[]> {
-        try {
-            const questions: Question[] = await this.getAllQuestions();
-            const sortedQuestions: Question[] = questions.sort((a, b) => new Date(a.lastModification).getTime() - new Date(b.lastModification).getTime());
-            return sortedQuestions;
-        } catch (error) {
-            throw error;
-        }
+        const questions: Question[] = await this.getAllQuestions();
+        const sortedQuestions: Question[] = questions.sort((a, b) => new Date(a.lastModification).getTime() - new Date(b.lastModification).getTime());
+        return sortedQuestions;
     }
 
-    async deleteGameByID(id: String): Promise<Boolean>{
-        try {
-            const questions: Question[] = await this.getAllQuestions();
-            const updatedQuestions: Question[] = questions.filter((question) => question.id !== id);
-            await fs.writeFile(QUESTIONS_PATH, JSON.stringify(updatedQuestions, null, 2), 'utf8');
-            return true;
-        } catch (error) {
-            throw error;
-        }
+    async addGame(game: Game): Promise<void> {
+        const games: Game[] = await this.getAllGames();
+        games.push(game);
+        await fs.writeFile(QUIZ_PATH, JSON.stringify(games, null, 2), 'utf8');
+    }
+
+    async getAllGames(): Promise<Game[]> {
+        const data: string = await fs.readFile(QUIZ_PATH, 'utf8');
+        const games: Game[] = JSON.parse(data);
+        return games;
+    }
+
+    async getGameByID(id: string): Promise<Game> {
+        const games: Game[] = await this.getAllGames();
+        return games.find((game) => game.id === id);
+    }
+
+    async toggleGameHidden(id: string): Promise<boolean> {
+        const games: Game[] = await this.getAllGames();
+        const updatedGames: Game[] = games.map((game) =>
+            game.id === id ? { ...game, lastModification: new Date(), isHidden: !game.isHidden } : game,
+        );
+        await fs.writeFile(QUIZ_PATH, JSON.stringify(updatedGames, null, 2), 'utf8');
+        return true;
+    }
+
+    async deleteGameByID(id: string): Promise<boolean> {
+        const games: Game[] = await this.getAllGames();
+        const updatedGames: Game[] = games.filter((game) => game.id !== id);
+        await fs.writeFile(QUIZ_PATH, JSON.stringify(updatedGames, null, 2), 'utf8');
+        return true;
     }
 }
-    
