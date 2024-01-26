da3095c56c09cbedd47ec9950b9a58aca369e5b9
fildesch <113048616+fildesch@users.noreply.github.com>
2024-01-26 13:35:53 -0500

    verification import complète

--- client/src/app/pages/admin-page/admin-page.component.ts
+++ client/src/app/pages/admin-page/admin-page.component.ts
@@ -77,40 +77,88 @@ export class AdminPageComponent {
         return this.selectedFile && this.selectedFile.type === 'application/json';
     }
 
-    handleFile(jsonArray: unknown) {
+    handleFile(jsonArray: any) {
         if (this.isGame(jsonArray)) {
-            const game: Game = jsonArray;
+            const game: Game = {id: this.getRandomID(), title: jsonArray.title, description: jsonArray.description, duration: jsonArray.duration, lastModification: new Date(), isHidden: true, questions: jsonArray.questions};
             this.http.post('http://localhost:3000/api/admin/importgame', { game }).subscribe((response: any) => {});
-        } else console.log(this.isGame(jsonArray));
+        }
+    }
+
+    getRandomID(): string {
+        let randomID: string = "";
+        for (let i = 0; i < 12; i++) {
+            randomID+=Math.floor(Math.random() * 10);
+        }
+        return randomID;
     }
 
     isArrayOfQuestions(questions: Question[]): questions is Question[] {
+        if (questions.length === 0) {
+            console.log("Le jeu doit contenir au moins une question.");
+        }
+        if (!questions.every((question: Question) => question.type === "QCM" || question.type === "QRL")) {
+            console.log("Les questions du jeu doivent être de type QCM ou QRL.")
+        }
+        if (!questions.every((question: Question ) => question.text && typeof question.text === "string")) {
+            console.log("Les questions doivent avoir un texte de type string.");
+        }
+        if (!questions.every((question: Question) => question.points && typeof question.points === "number" && (question.points >=10 && question.points <= 100 && question.points % 10 === 0))) {
+            console.log("Les questions doivent avoir un nombre de points alloué compris entre 10 et 100 et être un multiple de 10.")
+        }
+        if (!questions.every((question: Question) => question.choices.length >= 2 && question.choices.length <=4)) {
+            console.log("Les questions doivent contenir un nombre de choix compris entre 2 et 4.");
+        }
+        if (!questions.every((question: Question) => question.choices.every((choice: Choices) => choice.text && typeof choice.text === "string"))) {
+            console.log("Les choix de réponse des questions doivent avoir un texte de type string.")
+        }
+        
+
         return (
             Array.isArray(questions) &&
             questions.every(
                 (question: Question) =>
-                    typeof question.type === 'string' && // Adjusted to handle type as string
+                    typeof question.type === 'string' &&
                     (question.type === 'QCM' || question.type === 'QRL') &&
                     typeof question.text === 'string' &&
                     typeof question.points === 'number' &&
+                    (question.points >=10 && question.points <= 100 && question.points % 10 === 0) &&
+                    (question.choices.length >= 2 && question.choices.length <=4) &&
                     Array.isArray(question.choices) &&
                     question.choices.every(
-                        (choice: Choices) => typeof choice.text === 'string',
-                        // (typeof choice.isCorrect === 'boolean' || choice.isCorrect === null )
-                    ),
+                        (choice: Choices) => typeof choice.text === 'string'
+                    )
             )
         );
     }
 
     isGame(obj: any): obj is Game {
+        if (!obj.title || (typeof obj.title !== "string")) {
+            console.log("Le jeu importé doit avoir un titre de type string.");
+        }
+        else {
+            if (this.games.some(game => game.title === obj.title)) {
+                console.log("Le titre choisi existe déjà. Veuillez en choisir un nouveau.")
+            }
+        }
+        if (!obj.description || (typeof obj.description !== "string")) {
+            console.log("Le jeu importé doit avoir une description de type string.");
+        }
+        if (!obj.duration || (typeof obj.duration !== "number")) {
+            console.log("Le jeu importé doit avoir un temps alloué de type int.");
+        }
+        else {
+            if (obj.duration < 10 || obj.duration > 60) {
+                console.log("Le temps alloué pour une réponse doit être compris entre 10 et 60 secondes.")
+            }
+        }
+        
         return (
             obj &&
-            typeof obj.id === 'string' &&
             typeof obj.title === 'string' &&
             typeof obj.description === 'string' &&
             typeof obj.duration === 'number' &&
-            typeof obj.lastModification === 'string' &&
-            // typeof obj.isHidden === 'boolean' &&
+            !this.games.some(game => game.title === obj.title) &&
+            (obj.duration >= 10 && obj.duration <= 60) &&
             this.isArrayOfQuestions(obj.questions)
         );
     }
