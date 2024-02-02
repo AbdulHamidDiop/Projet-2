55b1014bbfe5b88ddbabc92734ef43f9a1d5f857
Youva <youva.boutora@polymtl.ca>
2024-02-02 10:45:04 -0500

    Ajout questions choix multiples.

--- client/src/app/components/play-area/play-area.component.ts
+++ client/src/app/components/play-area/play-area.component.ts
@@ -55,17 +55,79 @@ export class PlayAreaComponent {
         ],
     };
 
+<<<<<<< Updated upstream
     private isCorrect: boolean;
     private answer: string;
     private readonly timer = 5;
+=======
+    private isCorrect: boolean[] = [];
+    private answer: string[] = [];
+    private readonly timer = 25;
+>>>>>>> Stashed changes
     private points = 0;
     private score = 0;
     constructor(private readonly timeService: TimeService) {
         this.timeService.startTimer(this.timer);
+<<<<<<< Updated upstream
         this.isCorrect = false;
         this.answer = '';
     }
 
+=======
+        this.isCorrect = [];
+        this.answer = [];
+        //        this.questionService.getAllQuestions();
+        this.question = this.questionService.question;
+        this.nbChoices = this.question.choices.length;
+        const nbMaxQuestionsQCM = 10;
+        for (let i = this.question.choices.length; i < nbMaxQuestionsQCM; i++) {
+            this.question.choices.push({ text: '', isCorrect: false });
+        }
+    }
+
+    @HostListener('keydown', ['$event'])
+    buttonDetect(event: KeyboardEvent) {
+        this.buttonPressed = event.key;
+        if (this.buttonPressed === 'Enter') {
+            /* if (this.isCorrect && this.answer !== '') {
+                this.score += this.question.points;
+            }
+            this.timeService.stopTimer();
+            this.timeService.startTimer(this.timer);
+            this.answer = '';
+            this.isCorrect = false;*/
+            this.updateScore();
+        } else if (
+            this.buttonPressed >= '1' &&
+            this.buttonPressed <= '9' &&
+            this.question.type === Type.QCM &&
+            this.buttonPressed <= this.nbChoices.toString()
+        ) {
+            const index = parseInt(this.buttonPressed, 10);
+            this.handleQCMChoice(this.question.choices[index - 1].text, this.question.choices[index - 1].isCorrect);
+        }
+    }
+
+    shouldRender(text: string) {
+        return text !== '';
+    }
+
+    nextQuestion() {
+        this.answer = [];
+        this.isCorrect = [];
+        const question = this.questionService.question;
+        this.nbChoices = question.choices.length;
+        this.question.text = question.text;
+        for (let i = 0; i < question.choices.length; i++) {
+            this.question.choices[i].text = question.choices[i].text;
+            this.question.choices[i].isCorrect = question.choices[i].isCorrect;
+        }
+        for (let i = question.choices.length; i < this.question.choices.length; i++) {
+            this.question.choices[i].text = '';
+            this.question.choices[i].isCorrect = false;
+        }
+    }
+>>>>>>> Stashed changes
     // Devra être changé plus tard.
     get time(): number {
         if (this.timeService.time === 0) this.updateScore();
@@ -103,17 +165,38 @@ export class PlayAreaComponent {
     }
 
     handleQCMChoice(answer: string, isCorrect: boolean) {
-        if (answer === this.answer) {
-            this.answer = '';
-            this.isCorrect = false;
-        } else {
-            this.isCorrect = isCorrect;
-            this.answer = answer;
+        let choiceInList = false;
+        for (let i = 0; i < this.answer.length; i++) {
+            if (answer === this.answer[i]) {
+                this.answer.splice(i, 1);
+                this.isCorrect.splice(i, 1);
+                choiceInList = true;
+                i--;
+                break;
+            }
+            // else {
+            //                this.isCorrect &&= isCorrect;
+            //                this.answer[i] = answer;
+            // }
+        }
+        if (!choiceInList) {
+            this.answer.push(answer);
+            this.isCorrect.push(isCorrect);
         }
     }
 
     isChoice(choice: string): boolean {
-        return this.answer === choice;
+        for (const answer of this.answer) {
+            if (answer === choice) {
+                return true;
+            }
+        }
+        /*      for (let i = 0; i < this.answer.length; i++) {
+            if (this.answer[i] === choice) {
+                return true;
+            }
+        }*/
+        return false;
     }
 
     handleQRLAnswer(answer: string) {
@@ -127,13 +210,28 @@ export class PlayAreaComponent {
     }
 
     updateScore() {
-        if (this.isCorrect && this.answer !== '') {
+        let correctAnswer = false;
+        for (let i = 0; i < this.isCorrect.length && this.isCorrect.length === this.answer.length; i++) {
+            if (this.isCorrect[i]) {
+                correctAnswer = true;
+                // this.score += this.question.points;
+            } else {
+                correctAnswer = false;
+            }
+        }
+        if (correctAnswer) {
             this.score += this.question.points;
         }
         this.timeService.stopTimer();
         this.timeService.startTimer(this.timer);
+<<<<<<< Updated upstream
         this.answer = '';
         this.isCorrect = false;
+=======
+        this.answer = [];
+        this.isCorrect = [];
+        this.nextQuestion();
+>>>>>>> Stashed changes
     }
 
     // TODO : déplacer ceci dans un service de gestion de la souris!
@@ -145,7 +243,7 @@ export class PlayAreaComponent {
     }
 
     getStyle(choice: string) {
-        if (choice === this.answer) {
+        if (this.answer.length < 0 && choice !== '') {
             return 'selected';
         } else {
             return '';
