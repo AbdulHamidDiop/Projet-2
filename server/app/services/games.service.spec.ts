3585041ef4787bcbac090b2037334f896315ee35
aantem <alexandre.antem@polymtl.ca>
2024-02-06 23:15:00 -0500

    tests service  games server

--- server/app/services/games.service.spec.ts
+++ server/app/services/games.service.spec.ts
@@ -1,74 +1,164 @@
-// import { Game } from '@common/game';
-// import { expect } from 'chai';
-// import * as fs from 'fs/promises';
-// import * as sinon from 'sinon';
-// import { GamesService } from './games.service';
+import { Game } from '@common/game';
+import { expect } from 'chai';
+import fs from 'fs';
+import sinon from 'sinon';
+import { GamesService } from './games.service';
+const gamePath = './assets/test-data/quizs.json';
+const testDataLength = 11;
+const firstGame = {
+    id: '462778813469',
+    title: 'Trivia des top 50 artistes des années 2000',
+    description: 'Questaqions de pratique sur le langage JavaScript',
+    duration: 60,
+    lastModification: '2024-02-02T17:56:00.555Z',
+    isHidden: true,
+    questions: [
+        {
+            type: 'QCM',
+            text: 'Parmi les mots suivants, lesquels sont des mots clés réservés en JS?',
+            points: 40,
+            choices: [
+                {
+                    text: 'var',
+                    isCorrect: true,
+                },
+                {
+                    text: 'self',
+                    isCorrect: false,
+                },
+                {
+                    text: 'this',
+                    isCorrect: true,
+                },
+                {
+                    text: 'int',
+                },
+            ],
+        },
+        {
+            type: 'QCM',
+            text: "Est-ce qu'on le code suivant lance une erreur : const a = 1/NaN; ? ",
+            points: 20,
+            choices: [
+                {
+                    text: 'Non',
+                    isCorrect: true,
+                },
+                {
+                    text: 'Oui',
+                    isCorrect: false,
+                },
+            ],
+        },
+    ],
+};
 
-// const QUIZ_PATH = './assets/quiz-example.json';
+const testGame = {
+    id: '00000000-1111-2222-test-000000000000',
+    lastModification: '2024-02-02T15:28:59.795Z',
+    title: 'test',
+    description: 'test',
+    duration: 40,
+    questions: [
+        {
+            type: 'QCM',
+            text: 'Quelle est la différence entre NodeJS et Angular',
+            points: 20,
+            addToBank: true,
+            choices: [
+                {
+                    text: 'Angular = front-end, NodeJS = back-end',
+                    isCorrect: false,
+                },
+                {
+                    text: 'Angular = back-end, NodeJS = front-end',
+                    isCorrect: true,
+                },
+                {
+                    text: 'Aucune de ces réponses',
+                    isCorrect: false,
+                },
+            ],
+            id: 'e6547406-2543-4683-b0a2-dc0f1b01df66',
+            lastModification: '2024-01-25T16:09:35.649Z',
+        },
+    ],
+    isHidden: true,
+} as unknown as Game;
+describe('Games Service', () => {
+    let gamesService: GamesService;
+    let readFileStub: sinon.SinonStub;
+    let writeFileStub: sinon.SinonStub;
 
-// describe('GamesService', () => {
-//     let gamesService: GamesService;
-//     let readFileStub: sinon.SinonStub;
-//     let writeFileStub: sinon.SinonStub;
+    beforeEach(async () => {
+        gamesService = new GamesService();
+        readFileStub = sinon.stub(fs, 'readFile').callsFake((path, callback) => {
+            // Simulate reading data from a file
+            fs.readFile(gamePath, 'utf8', callback);
+        });
+        writeFileStub = sinon.stub(fs, 'writeFile').callsFake((path, data, callback) => {
+            // Simulate writing data to a file
+            fs.writeFile(gamePath, JSON.stringify(data, null, 2), 'utf8', callback);
+        });
+    });
 
-//     beforeEach(() => {
-//         gamesService = new GamesService();
-//         readFileStub = sinon.stub(fs, 'readFile');
-//         writeFileStub = sinon.stub(fs, 'writeFile');
-//     });
+    afterEach(() => {
+        readFileStub.restore();
+        writeFileStub.restore();
+    });
 
-//     afterEach(() => {
-//         sinon.restore();
-//     });
+    it('should get all games', async () => {
+        gamesService.getAllGames().then((games) => {
+            expect(games).to.be.an('array').with.lengthOf(testDataLength);
+            expect(games[0]).to.deep.equal(firstGame);
+        });
+    });
 
-//     it('should get all games', async () => {
-//         const testData = '[{"id": "1", "title": "Game 1", "questions": []}]';
-//         readFileStub.resolves(Buffer.from(testData, 'utf-8'));
+    it('should add a game to the database', async () => {
+        gamesService.addGame(testGame).then(() => {
+            gamesService.getAllGames().then((games) => {
+                expect(games)
+                    .to.be.an('array')
+                    .with.lengthOf(testDataLength + 1);
+                expect(games[testDataLength + 1]).to.deep.equal(testGame);
+            });
+        });
+    });
 
-//         const games: Game[] = await gamesService.getAllGames();
+    it('should get a game from the database based on its id', async () => {
+        gamesService.getGameByID(firstGame.id).then((game) => {
+            expect(game).to.deep.equal(firstGame);
+        });
+    });
 
-//         expect(games).to.be.an('array').with.lengthOf(1);
-//         expect(games[0]).to.deep.equal({
-//             id: '1',
-//             title: 'Game 1',
-//             questions: [],
-//         });
-//     });
+    it('should return null if the id is not in the database', async () => {
+        gamesService.getGameByID('fakeID').then((game) => {
+            expect(game).to.equal(null);
+        });
+    });
 
-//     it('should add a game', async () => {
-//         const testData = '[{"id": "1", "title": "Game 1", "questions" : "[]"}]';
-//         const newGame: Game = { id: '2', title: 'Game 2', questions: [] };
-//         readFileStub.resolves(testData);
+    it('should toggle a games isHidden in the database based on its id', async () => {
+        gamesService.toggleGameHidden(firstGame.id).then((success) => {
+            expect(success).to.equal(true);
+            gamesService.getGameByID(firstGame.id).then((game) => {
+                expect(game.id).to.equal(firstGame.id);
+                expect(game.title).to.equal(firstGame.title);
+                expect(game.isHidden).to.not.equal(firstGame.isHidden);
+                expect(game.lastModification).to.not.equal(firstGame.lastModification);
+            });
+        });
+    });
 
-//         await gamesService.addGame(newGame);
-
-//         sinon.assert.calledWith(writeFileStub, QUIZ_PATH, sinon.match(testData), 'utf8');
-//     });
-
-//     it('should get a game by ID', async () => {
-//         const testData = '[{"id": "1", "title": "Game 1", "questions" : "[]"}]';
-//         readFileStub.resolves(testData);
-
-//         const game: Game = await gamesService.getGameByID('1');
-
-//         expect(game).to.deep.equal({ id: '1', title: 'Game 1' });
-//     });
-
-//     it('should toggle game hidden status', async () => {
-//         const testData = '[{"id": "1", "title": "Game 1", "questions" : "[]"}]';
-//         readFileStub.resolves(testData);
-
-//         const result: boolean = await gamesService.toggleGameHidden('1');
-//         expect(result).to.equal(true);
-//         sinon.assert.calledWith(writeFileStub, QUIZ_PATH, sinon.match(testData), 'utf8');
-//     });
-
-//     it('should delete a game by ID', async () => {
-//         const testData = '[{"id": "1", "title": "Game 1", "questions" : "[]"}]';
-//         readFileStub.resolves(testData);
-
-//         const result: boolean = await gamesService.deleteGameByID('1');
-//         expect(result).to.equal(true);
-//         sinon.assert.calledWith(writeFileStub, QUIZ_PATH, sinon.match('[]'), 'utf8');
-//     });
-// });
+    it('should delete a game from the database based on its id', async () => {
+        gamesService.deleteGameByID(firstGame.id).then((success) => {
+            if (success) {
+                gamesService.getAllGames().then((games) => {
+                    expect(games)
+                        .to.be.an('array')
+                        .with.lengthOf(testDataLength - 1);
+                    expect(games[0]).to.not.deep.equal(firstGame);
+                });
+            }
+        });
+    });
+});
