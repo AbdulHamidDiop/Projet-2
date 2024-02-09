32b26ef527ce682ca196b7dd34e56fc2eedadb9f
AbdulHamidDiop <ahdiop68@gmail.com>
2024-02-09 15:38:58 -0500

    enlever duplicated class dans questions.services

--- client/src/app/services/questions.service.ts
+++ client/src/app/services/questions.service.ts
@@ -107,88 +107,8 @@ export class QuestionsService {
                 throw new Error('Réponse du serveur malformée');
             }
         } catch (error) {
-            console.error('Erreur lors de la vérification de la réponse:', error);
+            // console.error('Erreur lors de la vérification de la réponse:', error);
             return false;
         }
-    }
-}
-
-@Injectable({
-    providedIn: 'root',
-})
-export class QuestionsService {
-    deleteRequest = new EventEmitter<Question>();
-    questions: Question[] = [];
-    currentQuestionIndex: number = 0;
-
-    constructor() {
-        this.getAllQuestions().then((questions: Question[]) => {
-            this.questions = questions;
-        });
-    }
-    get question(): Question {
-        if (this.questions.length === 0) {
-            return {} as Question;
-        } else if (this.currentQuestionIndex + 1 === this.questions.length) {
-            return this.questions[this.currentQuestionIndex];
-        } else {
-            return this.questions[this.currentQuestionIndex++];
-        }
-    }
-
-    async getAllQuestions(): Promise<Question[]> {
-        const response = await fetch(API_URL + 'questions');
-        if (!response.ok) {
-            throw new Error(`Error: ${response.status}`);
-        }
-        const questions: Question[] = await response.json();
-        if (this.questions.length < questions.length) {
-            this.questions = questions;
-        }
-        return questions;
-    }
-
-    sortAllQuestions(): Question[] {
-        const sortedQuestions: Question[] = this.questions.sort(
-            (a, b) => new Date(a.lastModification).getTime() - new Date(b.lastModification).getTime(),
-        );
-        return sortedQuestions;
-    }
-    async addQuestion(question: Question): Promise<void> {
-        const response = await fetch(API_URL + 'questions/add', {
-            method: 'POST',
-            headers: {
-                // eslint-disable-next-line @typescript-eslint/naming-convention
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(question),
-        });
-        if (!response.ok) {
-            throw new Error(`Error: ${response.status}`);
-        }
-    }
-
-    async editQuestion(question: Question): Promise<void> {
-        const response = await fetch(API_URL + 'questions/edit', {
-            method: 'PUT',
-            headers: {
-                // eslint-disable-next-line @typescript-eslint/naming-convention
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(question),
-        });
-        if (!response.ok) {
-            throw new Error(`Error: ${response.status}`);
-        }
-    }
-
-    async deleteQuestion(question: Question): Promise<void> {
-        const response = await fetch(API_URL + 'questions/delete/' + question.id, {
-            method: 'DELETE',
-        });
-        if (!response.ok) {
-            throw new Error(`Error: ${response.status}`);
-        }
-        this.deleteRequest.emit(question);
     }
 }
