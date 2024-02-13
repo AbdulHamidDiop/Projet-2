dd86db29dd7400c388286707a9d82dec5eb17fc1
AbdulHamidDiop <ahdiop68@gmail.com>
2024-02-13 17:34:09 -0500

    implementation de mock fetchs dans admin page et questions page

--- client/src/app/services/fetch.service.spec.ts
+++ client/src/app/services/fetch.service.spec.ts
@@ -1,12 +1,36 @@
+/* eslint-disable prefer-arrow/prefer-arrow-functions */
 import { TestBed } from '@angular/core/testing';
-
 import { FetchService } from './fetch.service';
+
+async function fetchMock(): Promise<Response> {
+    const fakeResponse = new Response(
+        JSON.stringify({
+            data: 'fake data',
+        }),
+        {
+            status: 200,
+            // eslint-disable-next-line @typescript-eslint/naming-convention
+            headers: { 'Content-Type': 'application/json' },
+        },
+    );
+    return Promise.resolve(fakeResponse);
+}
 
 describe('FetchService', () => {
     let service: FetchService;
+    const fetchSpy = jasmine.createSpy().and.callFake(fetchMock);
 
     beforeEach(() => {
-        TestBed.configureTestingModule({});
+        TestBed.configureTestingModule({
+            providers: [
+                {
+                    provide: FetchService,
+                    useValue: {
+                        fetch: fetchSpy,
+                    },
+                },
+            ],
+        });
         service = TestBed.inject(FetchService);
     });
 
