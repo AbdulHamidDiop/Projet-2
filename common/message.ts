5c112f90dbbd74886d3735e413e1984574919490
Youva <youva.boutora@polymtl.ca>
2024-04-15 16:42:56 -0400

    Reduction complexité du code dans socket-events.ts

--- common/message.ts
+++ common/message.ts
@@ -16,11 +16,27 @@ export enum SystemMessages {
     ROOM_UNLOCKED = 'La salle est maintenant déverrouillée',
     PLAYER_JOINED = 'a rejoint la partie',
     PLAYER_LEFT = 'a quitté la partie',
+
     GAME_STARTED = 'Le jeu commence',
     GAME_ENDED = 'Le jeu se termine',
+    EXCLUDED_FROM_CHAT = 'Vous avez été exclu du clavardage',
+    INCLUDED_IN_CHAT = 'Vous pouvez à nouveau poster dans le clavardage',
 }
 
-export let ROOM_LOCKED_MESSAGE: ChatMessage = { // let instead of const so the timestamp can be updated
+export let INCLUDE_IN_CHAT_MESSAGE: ChatMessage = {
+    message: SystemMessages.INCLUDED_IN_CHAT,
+    author: SystemMessages.AUTHOR,
+    timeStamp: new Date().toLocaleTimeString(),
+};
+
+export let EXCLUDE_FROM_CHAT_MESSAGE: ChatMessage = {
+    author: SystemMessages.AUTHOR,
+    message: SystemMessages.EXCLUDED_FROM_CHAT,
+    timeStamp: new Date().toLocaleTimeString(),
+};
+
+export let ROOM_LOCKED_MESSAGE: ChatMessage = {
+    // let instead of const so the timestamp can be updated
     author: SystemMessages.AUTHOR,
     message: SystemMessages.ROOM_LOCKED,
     timeStamp: new Date().toLocaleTimeString(),
